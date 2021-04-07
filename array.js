a8f56222175ca7e82e5d34cf27a83a69758fd6e9
anishmoncy <54007108+anishmoncy@users.noreply.github.com>
2021-04-07 10:54:07 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,243 @@
+    var link =[
+    "Photos/frankfurtskyline.jpg",
+    "Photos/DSC04210.jpg",
+    "Photos/DSC04246.jpg",
+    "Photos/DSC01205.jpg",
+    "Photos/DSC04162_edited.jpg",
+    "Photos/Brussels.jpg",
+    "Photos/Berliner.jpg",
+    "Photos/NewYears.jpg",
+    "Photos/Zurich.jpg" ,
+    "Photos/Paris.jpg" ,
+    "Photos/Koln.jpg" ,
+    "Photos/Bonn.jpg" ,
+    "Photos/Munich.jpg" ,
+    "Photos/Heroesstatue.jpg" ,
+    "Photos/Budapest.jpg" ,
+    "Photos/TreeofLife.jpg" ,
+    "Photos/RHine.jpg", 
+    "Photos/Clock.jpg" ,
+    "Photos/Prague.jpg" ,
+   "Photos/DSC01780.jpg",
+   "Photos/Luzern.jpg",
+   "Photos/Titlis.jpg",
+   "Photos/Hallstatt.jpg",
+   "Photos/Nuremburg.jpg",
+   "Photos/Nymphenbergpalace.jpg",
+"Photos/HofsburgPalace.jpg",
+"Photos/EisenerSteg.jpg",
+"Photos/Grund.jpg",
+"Photos/Budapest_parliament.jpg",
+"Photos/Cintaquiare.jpg",
+"Photos/BudaandPest.jpg",
+"Photos/Atomium.jpg",
+"Photos/EuropaStatue.jpg",
+"Photos/Maria-Theresien-Platz.jpg",
+"Photos/Olso.jpg",
+"Photos/OlympiaPark.jpg",
+"Photos/LionMonument.jpg",
+"Photos/Luxembourgcrown.jpg",
+"Photos/RHinevalley.jpg",
+"Photos/Warsaw.jpg",
+"Photos/Trdelnik.jpg",
+"Photos/Sigmondscolumn.jpg",
+"Photos/Heidelbergcastle.jpg"
+]
+
+
+    
+    var description=["Frankfurt Skyline","Triberg Hosche Wasserfalle - Blackforest","Schwarzwald Museum",
+    "Manheim city","Felsenmeer","The Grand Place - Brussels","Berliner Aircraft Museum - Frankfurt",
+    "NewYearsEveAtBrussels","Zurich Old Town","Eiffel Tower - Paris","Koln Cathedral","Bonn","Merianplatz - Munich",
+    "Heroes square - Hungary","Hungary's Victim Complex","Jewish Synagogue - Budapest","Rhine waterfalls -Switzerland",
+    "Astronomical clock - Prague","Charles Bridge - Prague","Arc-De Triumph - Paris","Luzern - Switzerland","Mt.Titlis - Switerland","Hallstatt Skywalk - Austria","Nuremburg - Germany","The Nymphenberg palace - Munich",
+    "Hofsburg Palace - Vienna","Eisener Steg - Frankfurt","The Grund - Luxembourg","Hungarian Parliament - Budapest","Cintaquire - Brussels",
+"Buda & Pest","Atomium - Brusesels","The Europa statue - Brussels","Maria-Theresien-Platz - Vienna","Oslo By night - Norway","OlympiaPark - Munich",
+"The Lion Monument - Luzern","Luxembourgcrown","Rhine valley - Rudeshiem" ,"Warsaw - poland","Trdelnik or Chimney cake - Prague",
+"Sigmond's column - Warsaw","Heidelberg castle - Heidelberg"
+]
+
+    var imgArray = new Array()
+    
+
+    imgArray[0]=new Image()
+    imgArray[0].src="Photos/frankfurtskyline.jpg"
+    imgArray[0].alt="Frankfurt Skyline"
+
+    imgArray[1]=new Image()
+    imgArray[1].src="Photos/DSC04210.jpg"
+    imgArray[1].alt="Triberg Hosche Wasserfalle - Blackforest"
+
+    imgArray[2]=new Image()
+    imgArray[2].src="Photos/DSC04246.jpg"
+    imgArray[2].alt="Schwarzwald Museum - Triberg"
+
+
+    imgArray[3]=new Image()
+    imgArray[3].src="Photos/DSC01205.jpg"
+    imgArray[3].alt="Manheim city"
+
+
+    imgArray[4]=new Image()
+    imgArray[4].src="Photos/DSC04162_edited.jpg"
+    imgArray[4].alt="Felsenmeer"
+
+    
+    imgArray[5]=new Image()
+    imgArray[5].src="Photos/Brussels.jpg"
+    imgArray[5].alt="The Grand Place - Brussels"
+
+    
+    imgArray[6]=new Image()
+    imgArray[6].src="Photos/Berliner.jpg"
+    imgArray[6].alt="Berliner Aircraft Museum - Frankfurt"
+    
+    imgArray[7]=new Image()
+    imgArray[7].src="Photos/NewYears.jpg"
+    imgArray[7].alt="NewYearsEveAtBrussels"
+
+    
+    imgArray[8]=new Image()
+    imgArray[8].src="Photos/Zurich.jpg"
+    imgArray[8].alt="Zurich Old Town"
+
+    imgArray[9]=new Image()
+    imgArray[9].src="Photos/Paris.jpg"
+    imgArray[9].alt="Eiffel Tower - Paris"
+
+    
+ imgArray[10]=new Image()
+    imgArray[10].src="Photos/Koln.jpg"
+    imgArray[10].alt="Koln Cathedral"
+
+ imgArray[11]=new Image()
+    imgArray[11].src="Photos/Bonn.jpg"
+    imgArray[11].alt="Bonn"
+
+ imgArray[12]=new Image()
+    imgArray[12].src="Photos/Munich.jpg" 
+    imgArray[12].alt="Merianplatz - Munich"
+
+ imgArray[13]=new Image()
+    imgArray[13].src="Photos/Heroesstatue.jpg" 
+    imgArray[13].alt="Heroes square - Hungary"
+
+ imgArray[14]=new Image()
+    imgArray[14].src="Photos/Budapest.jpg"
+    imgArray[14].alt="Hungary's Victim Complex"
+
+ imgArray[15]=new Image()
+    imgArray[15].src="Photos/TreeofLife.jpg"
+    imgArray[15].alt="Jewish Synagogue - Budapest"
+
+ imgArray[16]=new Image()
+    imgArray[16].src="Photos/RHine.jpg"
+    imgArray[16].alt="Rhine waterfalls -Switzerland"
+
+ imgArray[17]=new Image()
+    imgArray[17].src="Photos/Clock.jpg"
+    imgArray[17].alt="Astronomical clock-Prague"
+
+ imgArray[18]=new Image()
+    imgArray[18].src="Photos/Prague.jpg"
+    imgArray[18].alt="Charles Bridge - Prague"
+
+ imgArray[19]=new Image()
+    imgArray[19].src="Photos/DSC01780.jpg" 
+    imgArray[19].alt="Arc-De Triumph - Paris"
+    
+    
+  imgArray[20]=new Image()
+    imgArray[20].src="Photos/Luzern.jpg"
+    imgArray[20].alt="Luzern"
+
+ imgArray[21]=new Image()
+    imgArray[21].src="Photos/Titlis.jpg"
+    imgArray[21].alt="Mt.Titlis"
+
+    imgArray[22]=new Image()
+    imgArray[22].src="Photos/Hallstatt.jpg"
+    imgArray[22].alt="Hallst"
+
+    
+    imgArray[23]=new Image()
+    imgArray[23].src="Photos/Nuremburg.jpg"
+    imgArray[23].alt="Nuremburg" 
+
+    imgArray[24]=new Image()
+    imgArray[24].src="Photos/Nymphenbergpalace.jpg"
+    imgArray[24].alt="Nymphberg castle" 
+    
+
+    imgArray[25]=new Image()
+    imgArray[25].src="Photos/HofsburgPalace.jpg"
+    imgArray[25].alt="Hofsburg Palace - Vienna"     
+
+
+    imgArray[26]=new Image()
+    imgArray[26].src="Photos/EisenerSteg.jpg"
+    imgArray[26].alt="EisenerSteg" 
+
+    imgArray[27]=new Image()
+    imgArray[27].src="Photos/Grund.jpg"
+    imgArray[27].alt="The Grund"
+
+    imgArray[28]=new Image()
+    imgArray[28].src="Photos/Budapest_parliament.jpg"
+    imgArray[28].alt="Hungarian Parliament" 
+
+    imgArray[29]=new Image()
+    imgArray[29].src="Photos/Cintaquiare.jpg"
+    imgArray[29].alt="Cintaquiare" 
+
+    imgArray[30]=new Image()
+    imgArray[30].src="Photos/BudaandPest.jpg"
+    imgArray[30].alt="Buda and Pest" 
+
+    imgArray[31]=new Image()
+    imgArray[31].src="Photos/Atomium.jpg"
+    imgArray[31].alt="Atomium" 
+
+    imgArray[32]=new Image()
+    imgArray[32].src="Photos/EuropaStatue.jpg"
+    imgArray[32].alt="EuropaStatue" 
+
+    imgArray[33]=new Image()
+    imgArray[33].src="Photos/Maria-Theresien-Platz.jpg"
+    imgArray[33].alt="Maria-Theresien-Platz" 
+
+    imgArray[34]=new Image()
+    imgArray[34].src="Photos/Oslo.jpg"
+    imgArray[34].alt="Oslo" 
+
+    imgArray[35]=new Image()
+    imgArray[35].src="Photos/OlympiaPark.jpg"
+    imgArray[35].alt="OlympiaPark - Munich" 
+
+    imgArray[36]=new Image()
+    imgArray[36].src="Photos/LionMonument.jpg"
+    imgArray[36].alt="The Lion Monument - Luzern" 
+
+    imgArray[37]=new Image()
+    imgArray[37].src="Photos/Luxembourgcrown.jpg"
+    imgArray[37].alt="Luxembourgcrown" 
+
+    imgArray[38]=new Image()
+    imgArray[38].src="Photos/RHinevalley.jpg"
+    imgArray[38].alt="Rhine valley - Rudeshiem" 
+
+    imgArray[39]=new Image()
+    imgArray[39].src="Photos/Warsaw.jpg"
+    imgArray[39].alt="Warsaw" 
+
+    imgArray[40]=new Image()
+    imgArray[40].src="Photos/Trdelnik.jpg",
+    imgArray[40].alt="Trdelnik or Chimney cake - Prague" 
+
+    imgArray[41]=new Image()
+    imgArray[41].src="Photos/Sigmondscolumn.jpg"
+    imgArray[41].alt="Sigmmond's Column" 
+
+    imgArray[42]=new Image()
+    imgArray[42].src="Photos/Heidelbergcastle.jpg"
+    imgArray[42].alt="Heidelberg castle" 
